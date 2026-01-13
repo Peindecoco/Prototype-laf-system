230fed63a673c79933b281d2336f5af619303fc6
Peindecoco <ezer.camacho10@gmail.com>
2026-01-13 23:04:30 +0800

    Update FoundItem.js

--- models/FoundItem.js
+++ models/FoundItem.js
@@ -3,8 +3,13 @@ import mongoose from "mongoose";
 const FoundItemSchema = new mongoose.Schema({
   name: String,
   description: String,
+  color: String,
+  size: String,
+  shape: String,
+  locationFound: String,
   imageUrl: String,
-  dateFound: { type: Date, default: Date.now }
+  dateFound: { type: Date, default: Date.now },
+  claimed: { type: Boolean, default: false }
 });
 
 export default mongoose.model("FoundItem", FoundItemSchema);
