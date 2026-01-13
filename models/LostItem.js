f9eac3454da28611107f9beecf1d8fe9c1e91ce7
Peindecoco <ezer.camacho10@gmail.com>
2026-01-13 22:55:53 +0800

    Update LostItem.js

--- models/LostItem.js
+++ models/LostItem.js
@@ -2,14 +2,11 @@ import mongoose from "mongoose";
 
 const LostItemSchema = new mongoose.Schema({
   itemName: String,
-  description: String,
+  locationLost: String,
   color: String,
   size: String,
   shape: String,
-  locationLost: String,
   secretDetail: String,
-  contact: String,
-  dateReported: { type: Date, default: Date.now },
   status: { type: String, default: "missing" }
 });
 
