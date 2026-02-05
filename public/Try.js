6aa448b574a84085ef858a698d1803762152bcf9
Peindecoco <ezer.camacho10@gmail.com>
2026-02-05 15:24:00 +0800

    Create Try.js

---
+++ public/Try.js
@@ -0,0 +1,14 @@
+<html>
+<head></head>
+<body>
+import OpenAI from "openai";
+const client = new OpenAI();
+
+const response = await client.responses.create({
+  model: "gpt-5.2",
+  input: "Write a short bedtime story about a unicorn.",
+});
+
+console.log(response.output_text);
+</body>
+</html>
